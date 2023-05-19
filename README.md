3451774e866cdf7d27fc21054f17c9bea85de6a5
Jeffrey <1115378579@qq.com>
2023-05-19 17:13:55 +0800

    feat: 添加获取电池信息

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 
+# device_battery
+
+- 获取设备电池信息
+- 只支持浏览器环境使用
+
+```shell
+
+npm install device_battery
+
+```
+```js
+import { device_battery } from 'device_battery'
+
+console.log(await device_battery)
+```
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-export const one = 1
-export const two = 2
+export async function device_battery() {
+  try {
+    // @ts-expect-error
+    const { level, charging, chargingTime, dischargingTime } = await navigator.getBattery()
+    const battery = {
+      isok: true,
+      level: `${level * 100}%`,
+      charging,
+      chargingTime,
+      dischargingTime,
+      description: 'Level: 电量百分百\n;charging: 充电状态\n;chargingTime: 完全充电需要的时间\n;dischargingTime: 完全放电需要的时间\n',
+
+    }
+    return battery
+  }
+  catch (e) {
+    console.log(e)
+    return {
+      isok: false,
+      description: '获取信息失败，可能是浏览器不兼容',
+    }
+  }
+}
+export default { device_battery }
